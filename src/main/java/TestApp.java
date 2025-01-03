d8cc65da9112768cf3a4258f2eccc162bd5a3ebf
JunKiBeom <dona0408@gmail.com>
2025-01-03 14:24:00 +0900

    TDD ing~

--- src/main/java/TestApp.java
+++
@@ -1,7 +0,0 @@
-public class TestApp {
-    public void run() {
-        System.out.println("aaa");
-
-        // 테스트에 써먹을려고
-    }
-}
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,10 +14,58 @@ public class FirstTest {
 
     @Test
     void t2() {
-        TestApp app = new TestApp();
-        app.run();
+//        App app = new App();
+//        app.run();
 
         // aaa가 출력되는가?
         // assertThat(result).isEqualTo("aaa");
+    }
+
+    @Test
+    void t3() {
+        String out = TestBot.run("");
+        assertThat(out)
+                .contains("명령 )")
+                .contains("명언앱을 종료합니다.");
+
+        // 출력값을 체크
+    }
+    @Test
+    @DisplayName("명령을 여러번 입력할 수 있다.")
+    void t4() {
+        String out = TestBot.run("""
+                등록
+                등록
+                종료
+                """);
+
+        // "명령 )"의 출현 횟수를 계산
+        long count = out.split("명령 \\)").length - 1;
+        // 검증
+        assertThat(count).isEqualTo(3); // 기대하는 횟수에 따라 값 수정
+    }
+
+    @Test
+    @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
+    void t5() {
+        String out = TestBot.run("");
+
+        assertThat(out)
+                .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
+
+    }
+
+    @Test
+    @DisplayName("등록 - 명언 1개 입력")
+    void t6() {
+        String out = TestBot.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(out)
+                .containsSubsequence("명언 : ", "작가 : ");
+
     }
 }
